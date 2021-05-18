f4acd7f59df66d01f877698676d1d75c24c9e16a
yuseonkim <yusun715@icloud.com>
2021-05-18 15:19:23 +0900

    StopListSet.java

--- src/main/java/Parse/StopListSet.java
+++ src/main/java/Parse/StopListSet.java
@@ -1,42 +1,36 @@
-package Parse;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+
 import java.io.IOException;
+import java.util.HashMap;
 
 public class StopListSet {
-    public StopList[] StopLists;
-    public int size;
+    public static HashMap<Integer, StopList> StopLists;
+    public static int size;
 
-    public StopListSet() throws IOException, org.json.simple.parser.ParseException {
+    public static HashMap<Integer, StopList> getStopLists() throws IOException, ParseException {
+        if (StopLists != null) return StopLists;
+
         JSONObject jsonObject = new GetApiData("STATION").getData();
-        JSONArray jsonArray=(JSONArray) jsonObject.get("STATION_LIST");
-        size=Integer.parseInt(jsonObject.get("ROW_COUNT").toString());
-        StopLists = new StopList[size];
-        for(int i=0;i<size;i++)
-            StopLists[i] = new StopList((JSONObject) jsonArray.get(i));
-
-        }
-        public void StopListPrint(){
-        for(int i =0;i<size;i++)
-        System.out.println("정류장번호: " +StopLists[i].getCurStopId() +" 현재 정류장 이름: "+StopLists[i].getCurStopName()+" 다음 정류장 이름: " +StopLists[i].getNextStopName());
+        JSONArray jsonArray = (JSONArray) jsonObject.get("STATION_LIST");
+        size = Integer.parseInt(jsonObject.get("ROW_COUNT").toString());
+        StopLists =new HashMap<>();
+        for(int i=0;i<size;i++){
+            StopList a = new StopList((JSONObject) jsonArray.get(i));
+            StopLists.put(a.curStopId, a);
         }
 
-//        public String getStop(int id){
-//
-//        }
+        return StopLists;
+    }
 
+    public static void StopListPrint(HashMap<Integer, StopList> StopLists) {
+        if (StopLists != null)
+            for (int i = 0; i < 9999; i++)
+                if (StopLists.get(i) != null)
+                    System.out.println("정류장번호:" + StopLists.get(i).curStopId + "현제 정류장:" + StopLists.get(i).curStopName + "다음 정류장:" + StopLists.get(i).nextStopName);
+
+
+    }
 }
-     /*for(int i =0;i<3;i++){
-            JSONObject jsonObjectSub = (JSONObject)  jsonArray.get(i);
-            String busNO = jsonObjectSub.get("BUSSTOP_NAME").toString();
-            System.out.println(busNO);*/
-
-
-
-//"RESULT_CODE":"SUCCSS"},"STATION_LIST":[{"STATION_NUM":1,
-// "BUSSTOP_NAME":"동원촌",
-// "ARS_ID":"5396",
-// "NEXT_BUSSTOP":"비아동주민센터(앞)",
-
 
